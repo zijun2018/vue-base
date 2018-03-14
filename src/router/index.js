2976c83713434381a9fc4bb552df19ac7e22d4b0
daijun <zijun2030@gmail.com>
2018-03-14 11:51:46 +0800

    路由更改为history模式；优化滚动行为

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,18 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  // HTML5 History 模式
+  mode: 'history',
+  // 滚动行为设置：先模拟锚点，再模拟记录位置，否则滚动到页面顶部
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return { selector: to.hash }
+    } else if(savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
